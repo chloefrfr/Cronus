25c00faeae5928e27898dc5f76a874bed6b482d7
chloe <118663359+chloefrfr@users.noreply.github.com>
2024-10-17 02:58:18 -0400

    Create ProfileController.cs

---
+++ Larry/Source/Controllers/ProfileController/ProfileController.cs
@@ -0,0 +1,21 @@
+﻿using Larry.Source.Utilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Larry.Source.Controllers.ProfileController
+{
+    [ApiController]
+    [Route("fortnite/api/game/v2/profile")]
+    public class ProfileController : Controller
+    {
+        [HttpPost("{accountId}/client/{operation}")]
+        public async Task<IActionResult> ProfileHandler(string accountId, string operation)
+        {
+            Logger.Information($"Operation => {operation}");
+
+
+
+            return Ok();
+
+        }
+    }
+}
